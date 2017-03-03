2647e7acb6889dc35f0329bff85aa2667b50d94b
Egor Gromyko <egorgromyko@gmail.com>
2017-03-03 13:35:28 +0100

    add first basic styles

--- app/app.js
+++ app/app.js
@@ -11,10 +11,6 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/css/style.css', function (req, res) {
-  res.send('you failed');
-});
-
 var server = app.listen(3000, function() {
   console.log('example app listening on port 3000!');
 });
