9ab8b6572047631b74e885eb1f3e50d234690b5d
Egor Gromyko <egorgromyko@gmail.com>
2017-03-03 13:08:55 +0100

    add public file directory

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.use('/public', express.static('./app/public'))
+
+
 app.set('view engine', 'ejs');
 app.set('views', './app/views/');
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get('/css/style.css', function (req, res) {
+  res.send('you failed');
 });
 
 var server = app.listen(3000, function() {
