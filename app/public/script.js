f00d3d0f93b0345f2cc1c918b7924f517ee2064f
Egor Gromyko <egorgromyko@gmail.com>
2017-03-03 15:14:11 +0100

    add song name message emission

--- app/public/script.js
+++ app/public/script.js
@@ -1,14 +1,17 @@
 var socket = io();
-
-socket.on('connect', function () {});
-socket.on('event', function () {});
-socket.on('disconnect', function () {});
-
-
 var songs = document.querySelector('.song-list').children;
 
-Object.keys(songs).forEach (function (key) {
-  songs[key].addEventListener('click', function (e) {
-    console.log(this);
-  })
+socket.on('connect', function () {
+
+  Object.keys(songs).forEach (function (key) {
+    songs[key].addEventListener('click', function (e) {
+
+      socket.emit('playSong', {
+        songName : this.textContent
+      })
+    })
+  });
 });
+
+socket.on('event', function () {});
+socket.on('disconnect', function () {});
